d6088b696c11cea01a729cbe30efb5ea5a1c2053
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-06 11:08:03 -0400

    Task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,7 +18,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	else if (tree->right != NULL || tree->left != NULL)
+	if (tree->right != NULL || tree->left != NULL)
 	{
 		return (1);
 	}
