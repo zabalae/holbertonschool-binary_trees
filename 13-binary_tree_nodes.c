ee057cbdff0532b9c1a1751b204abfa60f2fa521
Abdiel-88 <abdiel.f@outlook.com>
2024-01-07 14:18:44 -0400

    task 13 fix?

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,31 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - function that counts the nodes with
- * at least 1 child in a binary tree
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+ * @tree: Pointer to the root node of the tree to count the number of nodes
  *
- * @tree: pointer to the root node of the tree to count the number of nodes
- *
- * Return: number of nodes
+ * Return: Number of nodes with at least 1 child. If tree is NULL, return 0.
  */
-
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
+	size_t count = 0;
 
+	/* Return 0 if tree is NULL */
 	if (tree == NULL)
-	{
-		return (nodes);
-	}
+		return (0);
 
-	nodes++;
+	/* Count the current node if it has at least one child */
+	if (tree->left != NULL || tree->right != NULL)
+		count = 1;
 
-	if (tree->right != NULL || tree->left != NULL)
-	{
-		nodes--;
-		nodes += binary_tree_nodes(tree->right);
-		nodes += binary_tree_nodes(tree->left);
-	}
-
-	return (nodes);
+	/* Recursively count nodes in left and right subtrees */
+	return (count + binary_tree_nodes(tree->left)
+		+ binary_tree_nodes(tree->right));
 }
