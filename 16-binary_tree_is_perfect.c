121e33ea9a92b778933042f15228ec1cae78ca3e
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-08 09:24:22 -0400

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,7 +22,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (left_height == right_height)
 	{
 		return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right);
+			binary_tree_is_perfect(tree->right));
 	}
 
 	return (0);
