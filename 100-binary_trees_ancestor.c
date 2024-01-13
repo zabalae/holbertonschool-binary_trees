7917189df139a0b708c2c77ab8f4f10465ad9b16
Elizabeth <elizabeth.zabala3@gmail.com>
2024-01-13 18:22:49 -0400

    Task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,4 +11,26 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
+	binary_tree_t *ancestor1, *ancestor2;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	ancestor1 = first->parent;
+	ancestor2 = second->parent;
+
+	if (first == ancestor2 || !ancestor1)
+	{
+		return (binary_trees_ancestor(first, ancestor2));
+	}
+
+	if (ancestor1 == second || !pop)
+	{
+		return (binary_trees_ancestor(second, ancestor1))
+	}
+
+	return (binary_trees_ancestor(ancestor1, ancestor2));
 }
